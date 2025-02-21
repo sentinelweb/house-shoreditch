c54457e3b9a42274c665cb038567a341c71da0b0
sentinelweb <robmunro@robs-MacBook-Pro.local>
2025-02-21 19:10:44 +0100

    19 enable test for win msi build

--- composeApp/src/commonMain/kotlin/com/house_shoreditch/app/main/TestUrl.kt
+++ composeApp/src/commonMain/kotlin/com/house_shoreditch/app/main/TestUrl.kt
@@ -5,23 +5,14 @@ import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 
+// testing code for internet connection
 class TestUrl {
     suspend fun get(url:String) {
         val client = HttpClient(CIO)
 
         try {
-            // Specify the URL of the file
-            //val fileUrl = "https://example.com/sample-file.txt"
-            // The path where the file will be saved
-            //val filePath = "downloaded-file.txt"
-
-            // Make the GET request and write the response to a file
             client.get(url) {
-                //val file = File(filePath)
-//                file.writeBytes(responseBytes)
-//                println("File downloaded successfully to ${file.absolutePath}")
-            }
-                .also { println(it.body<String>()) }
+            }.also { println(it.body<String>()) }
         } catch (e: Exception) {
             println("File download failed: ${e.message}")
         } finally {
